0f99581149ff1f61734b5079943857aa83b2f4d4
Andy Damevin <ia3andy@gmail.com>
2013-04-11 14:54:56 +0200

    First working version

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,65 +9,48 @@
 'use strict';
 
 module.exports = function(grunt) {
-
-  // Project configuration.
   grunt.initConfig({
     jshint: {
       all: [
         'Gruntfile.js',
         'tasks/*.js',
-        '<%= nodeunit.tests %>',
+        '<%= nodeunit.tests %>'
       ],
       options: {
-        jshintrc: '.jshintrc',
-      },
+        jshintrc: '.jshintrc'
+      }
     },
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
-    },
-
-    // Configuration to be run (and then tested).
-    nodestatic: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!',
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-    },
-
-    // Unit tests.
     nodeunit: {
-      tests: ['test/*_test.js'],
+      tests: ['test/*_test.js']
     },
 
+    nodestatic: {
+      custom_base: {
+        options: {
+          base: 'test'
+        },
+      },
+      custom_port: {
+        options: {
+          port: 9000,
+        },
+      },
+      dev_move: {
+        options: {
+          port: 9001,
+          dev: true
+        },
+      },
+    }
   });
 
-  // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-internal');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'nodestatic', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
-
+  grunt.registerTask('test', ['nodestatic', 'nodeunit']);
+  grunt.registerTask('default', ['jshint', 'test', 'build-contrib']);
 };
--- tasks/nodestatic.js
+++ tasks/nodestatic.js
@@ -1,50 +1,92 @@
 /*
- * grunt-nodestatic
- * https://github.com/ia3andy/grunt-nodestatic
+ * grunt-static
+ * https://github.com/ia3andy/grunt-static
  *
- * Copyright (c) 2013 Andy Damevin
  * Licensed under the MIT license.
  */
 
 'use strict';
 
 module.exports = function(grunt) {
+  var nodestatic = require('node-static'),
+      tty = require('tty'),
+      colors = require('colors');
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
-
-  grunt.registerMultiTask('nodestatic', 'Your task description goes here.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
+  grunt.registerMultiTask('nodestatic', 'Start a static web server.', function() {
+    // Merge task-specific options with these defaults.
     var options = this.options({
-      punctuation: '.',
-      separator: ', '
+      port: 8080,
+      base: '.',
+      keepalive: false,
+      cache: 7200,
+      dev: false,
+      headers: {},
+      serverInfo: "grunt-static",
+      verbose: true
     });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+    // Start server.
+    var done = this.async();
+    var taskTarget = this.target;
+    var keepAlive = this.flags.keepalive || options.keepalive;
 
-      // Handle options.
-      src += options.punctuation;
+    var serverOptions = {
+      headers: options.headers,
+      cache: options.cache,
+      serverInfo: options.serverInfo
+    };
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+    if(options.nocache){
+      if(serverOptions.headers == null){
+        serverOptions.headers = {}
+      }
+      serverOptions.headers["Cache-Control"] = "no-cache, must-revalidate"
+    }
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+    var log = function(request, response, statusCode) {
+        var d = new Date();
+        var seconds = d.getSeconds() < 10? '0'+d.getSeconds() : d.getSeconds(),
+            datestr = d.getHours() + ':' + d.getMinutes() + ':' + seconds,
+            line = datestr + ' [' + response.statusCode + ']: ' + request.url + "\n",
+            colorized = line;
+        if (tty.isatty(process.stdout.fd))
+            colorized = (response.statusCode >= 500) ? line.red.bold :
+                        (response.statusCode >= 400) ? line.red :
+                        line;
+        grunt.log.write(colorized);
+    };
+
+
+    var fileServer = new nodestatic.Server(options.base, serverOptions)
+    require('http').createServer(function (request, response) {
+        request.addListener('end', function () {
+            fileServer.serve(request, response, function(e, rsp) {
+                if (e && e.status === 404) {
+                    response.writeHead(e.status, e.headers);
+                    response.end("404 Not found");
+                    log(request, response);
+                } else if (e && e.code === 'EADDRINUSE') {
+                    grunt.fatal('Port ' + options.port + ' is already in use by another process.');
+                } else if (options.verbose) {
+                    log(request, response);
+                }
+            });
+        });
+    }).listen(options.port)
+    .on('listening', function() {
+      grunt.log.writeln('Serving ' + options.base + ' on port ' + options.port + '.');
+      grunt.config.set('nodestatic.' + taskTarget + '.options.port', options.port);
+
+      if (!keepAlive) {
+        done();
+      }
     });
+
+
+    if (keepAlive) {
+      // This is now an async task. Since we don't call the "done"
+      // function, this task will never, ever, ever terminate. Have fun!
+      grunt.log.write('Waiting forever...\n');
+    }
   });
-
 };
